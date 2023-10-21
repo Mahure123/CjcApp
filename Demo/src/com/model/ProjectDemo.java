a42e0537a7fc013b39794e184374171775eeceb2
Mahure123 <148521886+Mahure123@users.noreply.github.com>
2023-10-21 18:01:21 +0530

    Update ProjectDemo.java
    I have Updated line no 7 so check this

--- Demo/src/com/model/ProjectDemo.java
+++ Demo/src/com/model/ProjectDemo.java
@@ -4,6 +4,7 @@ public class ProjectDemo {
 	
 	public static void main(String[] args) {
 		System.out.println("This is my first project on GitHub");
+System.out.println("Updation happend in GitHub");
 	}
 
 }
